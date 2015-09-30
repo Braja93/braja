9ac23b5783319c36d7137c3a79b15ee259af6cef
nladwa <nitin.ladwa@gmail.com>
2015-09-30 13:33:31 +0100

    Added defaults to arguments, so you can just pass lm.contribution to lm.contribution.plot - columns should be in the right places.

--- R/lm.contribution.ggplot.R
+++ R/lm.contribution.ggplot.R
@@ -1,15 +1,18 @@
 #' Plots the contribution of a lm's independent variables
 #'
-#' Takes lm.contribution() output and visualises it.
-#' @param lm.contibution.object The output of lm.contribution()
-#' @param date.index The date index of the lm(data = ?) variable, as a character, e.g. "Week"
-#' @param dependent.var The dependent variable of the lm
+#' Takes lm.contribution output and visualises it.
+#' @param lm.contibution.object The output of lm.contribution.
+#' @param date.index Only define if error. The date index of the lm(data = ?) variable, as a character, e.g. "Week".
+#' @param dependent.var Only define if error. The dependent variable of the lm.
 #' @keywords lm contribution ggplot
 #' @export
 #' @examples
 #' lm.contribution.ggplot()
 
-lm.contribution.ggplot <- function(lm.contribution.object, date.index, dependent.var) {
+lm.contribution.ggplot <- function(lm.contribution.object, date.index = NULL, dependent.var = NULL) {
+  
+  if(is.null(date.index)) {date.index <- names(lm.contribution.object)[1]}
+  if(is.null(dependent.var)) {dependent.var <- names(lm.contribution.object)[2]}
   
   m.out <- reshape2::melt(lm.contribution.object, id.vars = date.index)
   
