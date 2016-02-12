d12dd65ee66ad092d42aa14cc2af21e77deaa646
nladwa <nitin.ladwa@gmail.com>
2016-02-12 13:15:28 +0000

    Made melt errors silent.

--- R/lm.actualvfitted.ggplot.R
+++ R/lm.actualvfitted.ggplot.R
@@ -8,7 +8,11 @@
 
 lm.actualvfitted.ggplot <- function(lm.actualvfitted.object) {
   id.var <- names(lm.actualvfitted.object)[1]
+  
+  options(warn = -1)
   out <- reshape2::melt(lm.actualvfitted.object, id.vars = id.var)
-  ggplot2::ggplot(out, ggplot2::aes_string(x = id.var, y = "value", colour = "variable")) +
+  options(warn = 0)
+  
+  ggplot2::ggplot(out, ggplot2::aes_string(x = id.var, y = "value", colour = "variable", group = "variable")) +
     ggplot2::geom_line()
 }
--- R/lm.contribution.ggplot.R
+++ R/lm.contribution.ggplot.R
@@ -9,23 +9,28 @@
 #' @example inst/examples/lm.contribution.ggplot.R
 
 lm.contribution.ggplot <- function (lm.contribution.object, date.index = NULL, dependent.var = NULL) {
+  
   if (is.null(date.index)) {
     date.index <- names(lm.contribution.object)[1]
   }
+  
   if (is.null(dependent.var)) {
     dependent.var <- names(lm.contribution.object)[2]
   }
+  
+  options(warn = -1)
   m.out <- reshape2::melt(lm.contribution.object, id.vars = date.index)
+  options(warn = 0)
   
   if (m.out[variable != dependent.var, min(value)] >= 0) {
     lm.plot <- ggplot2::ggplot() +
-      ggplot2::geom_area(data = m.out[variable != dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable")) +
-      ggplot2::geom_line(data = m.out[variable == dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable"))
+      ggplot2::geom_area(data = m.out[variable != dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable", group = "variable")) +
+      ggplot2::geom_line(data = m.out[variable == dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable", group = "variable"))
   } else {
     lm.plot <- ggplot2::ggplot() +
-      ggplot2::geom_area(data = m.out[value >= 0 & variable != dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable")) +
-      ggplot2::geom_area(data = m.out[value < 0 & variable != dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable")) +
-      ggplot2::geom_line(data = m.out[variable == dependent.var], ggplot2::aes_string(x = date.index, y = "value"), colour= "black")
+      ggplot2::geom_area(data = m.out[value >= 0 & variable != dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable", group = "variable")) +
+      ggplot2::geom_area(data = m.out[value < 0 & variable != dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable", group = "variable")) +
+      ggplot2::geom_line(data = m.out[variable == dependent.var], ggplot2::aes_string(x = date.index, y = "value", group = "variable"), colour = "black")
   }
   
   print(lm.plot)
