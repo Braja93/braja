cf8273a47cca186a9112f498aa2bdb2230cf2dd0
nladwa <nitin.ladwa@gmail.com>
2016-04-06 15:30:33 +0100

    Updated examples.

--- inst/examples/adstock.multiple.R
+++ inst/examples/adstock.multiple.R
@@ -1,8 +1,8 @@
 x <- data.table::fread(system.file("extdata", "TVRs.csv", package = "nladwa"))
 x[, Week := as.Date(Week, "%d/%m/%Y")]
 
-out1 <- adstock(data = x, variable = "TVRs", adstock.rates = 0.1)
-out2 <- adstock(data = x, variable = "TVRs", adstock.rates = seq(from = 0.1, to = 0.9, by = 0.1))
+out1 <- adstock.multiple(data = x, variable = "TVRs", adstock.rates = 0.1)
+out2 <- adstock.multiple(data = x, variable = "TVRs", adstock.rates = seq(from = 0.1, to = 0.9, by = 0.1))
 
 plot.out1 <- data.table::melt(out1, id.vars = "Week")
 
