e9c556ba91a6cebd1183fde76b412ad6cfe3e61e
nladwa <nitin.ladwa@gmail.com>
2016-09-06 12:29:58 +0100

    freadExcel. New function to read XLSX as data.table.

---
+++ R/freadExcel.R
@@ -0,0 +1,25 @@
+#' Reads Excel table from file to data.table
+#'
+#' Create data.table from Excel table that's in an XLSX file.
+#' @param file Character. Path to file.
+#' @param date.index Character. Optional, column name of date variable.
+#' @param sheet Integer. Sheet number of table (from open.xlsx)
+#' @param startRow Integer. Row that the data starts (from open.xlsx)
+#' @param origin Character. from open.xlsx
+#' @keywords excel xlsx fread
+#' @export
+
+freadExcel <- function(file, date.index = NULL, sheet = 1, startRow = 1, origin = "1900-01-01") {
+  
+  out <- data.table::as.data.table(
+    openxlsx::read.xlsx(xlsxFile = file, sheet = sheet, startRow = startRow))
+  
+  if(is.null(date.index)) {
+    message("No date.index selected.")
+  } else {
+    out[, date.index := openxlsx::convertToDate(get(date.index), origin = origin), with = FALSE]
+  }
+  
+  return(out)
+  
+}
