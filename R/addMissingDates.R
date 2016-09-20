06cac625280e30e2314da0ad16a0d828cc17c7de
nladwa <nitin.ladwa@gmail.com>
2016-09-20 17:28:08 +0100

    Updated addMissingDates, args to include start/end date.

--- R/addMissingDates.R
+++ R/addMissingDates.R
@@ -3,23 +3,32 @@
 #' Adds rows with 0 values to a wide table that doesn't have consecutive dates
 #' @param x data.table. In wide format.
 #' @param date.index Character. The column in x that is the date variable.
-#' @param seq.by Integer. Number of days that each row should cover? 1 = week day, 7 = one week.
+#' @param start.date Character in "yyyy-mm-dd" format. The first date of the required output, if NULL, function takes the min of date.index.
+#' @param end.date Character in "yyyy-mm-dd" format. The last date of the required output, if NULL, function takes the max of date.index.
+#' @param data.frequency Integer. Number of days that each row should cover, 1 = one day, 7 = one week.
 #' @keywords missing dates
 #' @import data.table
 #' @export
 #' @example inst/examples/addMissingDates.R
 
-addMissingDates <- function(x, date.index, seq.by = 1) {
+addMissingDates <- function(x, date.index, start.date = NULL, end.date = NULL, data.frequency = 7) {
   
+  if(is.null(start.date)) start.date <- min(x[, get(date.index)])
+  if(is.null(end.date)) end.date <- max(x[, get(date.index)])
+  
+  start.date <- as.Date(start.date)
+  end.date <- as.Date(end.date)
+  
+  x <- data.table::rbindlist(list(x, x[NA], x[NA]))
+  x[(nrow(x) - 1):nrow(x), Week := c(start.date, end.date)]
   x.dates <- x[, get(date.index)]
-  tomerge.date <- seq(from = min(x.dates), to = max(x.dates), by = seq.by)
+  tomerge.date <- seq(from = start.date, to = end.date, by = data.frequency)
   tomerge.date <- tomerge.date[!(tomerge.date %in% x.dates)]
-  
   tomerge <- data.table::as.data.table(matrix(nrow = length(tomerge.date), ncol = ncol(x), dimnames = list(NULL, names(x)), data = 0))
-  tomerge[, date.index := tomerge.date, with = FALSE]
-  
+  tomerge[, `:=`(date.index, tomerge.date), with = FALSE]
   out <- data.table::rbindlist(list(x, tomerge))
   out <- out[, lapply(.SD, sum, na.rm = TRUE), by = date.index]
   data.table::setkeyv(out, date.index)
+  
   return(out)
 }
--- inst/examples/addMissingDates.R
+++ inst/examples/addMissingDates.R
@@ -1,4 +1,5 @@
 x <- data.table::fread(system.file("extdata", "Newspapers.csv", package = "nladwa"))
 x$Date <- as.Date(x$Date)
 print(x) # missing 2015-01-03, 05, 06, 07, 08, 09
-addMissingDates(x = x, date.index = "Date") # now has them all
+addMissingDates(x = x, date.index = "Date", data.frequency = 1) # now has them all
+addMissingDates(x = x, date.index = "Date", data.frequency = 1, start.date = "2014-12-01")
