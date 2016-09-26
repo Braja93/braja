763420e04b186558cfec62971e12e62aa4e76e27
nladwa <nitin.ladwa@gmail.com>
2016-09-26 12:18:01 +0100

    residPlot - make sure it's using complete cases.

--- R/residPlot.R
+++ R/residPlot.R
@@ -12,16 +12,18 @@ residPlot <- function(lm.object, date.index) {
   
   lm.data <- data.table(fortify(lm.object))
   data.source <- as.character(lm.object$call[3])
+  data.cols <- names(lm.object$model)
   
-  date.data <- get(data.source)[complete.cases(get(data.source)), get(date.index)]
-  lm.data[, Week := date.data]
+  date.data <- get(data.source)[, c(date.index, data.cols), with = FALSE]
+  date.data <- date.data[complete.cases(date.data), get(date.index)]
+  lm.data[, `:=`(Week, date.data)]
   
-  resid.plot <- ggplot(lm.data, aes_string(date.index, ".resid")) +
-    geom_point() +
-    geom_hline(yintercept = 0) +
-    geom_smooth(se = FALSE) +
+  resid.plot <- ggplot(lm.data, aes_string(date.index, ".resid")) + 
+    geom_point() + geom_hline(yintercept = 0) + geom_smooth(se = FALSE) + 
     labs(list(y = "Residuals"))
   
   print(resid.plot)
   
 }
+
+
