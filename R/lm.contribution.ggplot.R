50dd11a202d8d42880e2e048d181f66a6f79702a
nladwa <nitin.ladwa@gmail.com>
2016-11-21 16:26:12 +0000

    lm.contribution.ggplot. Changed from area plot to bar.

--- R/lm.contribution.ggplot.R
+++ R/lm.contribution.ggplot.R
@@ -9,32 +9,29 @@
 #' @export
 #' @example inst/examples/lm.contribution.ggplot.R
 
-lm.contribution.ggplot <- function (lm.contribution.object, date.index = NULL, dependent.var = NULL) {
-  
+lm.contribution.ggplot <- function(lm.contribution.object, date.index = NULL, dependent.var = NULL) 
+{
   if (is.null(date.index)) {
     date.index <- names(lm.contribution.object)[1]
   }
-  
   if (is.null(dependent.var)) {
     dependent.var <- names(lm.contribution.object)[2]
   }
-  
   options(warn = -1)
   m.out <- reshape2::melt(lm.contribution.object, id.vars = date.index)
   options(warn = 0)
-  
   if (m.out[variable != dependent.var, min(value)] >= 0) {
     lm.plot <- ggplot2::ggplot() +
-      ggplot2::geom_area(data = m.out[variable != dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable", group = "variable")) +
+      ggplot2::geom_bar(data = m.out[variable != dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable", group = "variable"), stat = "identity") +
       ggplot2::geom_line(data = m.out[variable == dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable", group = "variable")) +
       ggplot2::ylab(dependent.var)
-  } else {
+  }
+  else {
     lm.plot <- ggplot2::ggplot() +
-      ggplot2::geom_area(data = m.out[value >= 0 & variable != dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable", group = "variable")) +
-      ggplot2::geom_area(data = m.out[value < 0 & variable != dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable", group = "variable")) +
+      ggplot2::geom_bar(data = m.out[value >= 0 & variable != dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable", group = "variable"), stat = "identity") +
+      ggplot2::geom_bar(data = m.out[value < 0 & variable != dependent.var], ggplot2::aes_string(x = date.index, y = "value", fill = "variable", group = "variable"), stat = "identity") +
       ggplot2::geom_line(data = m.out[variable == dependent.var], ggplot2::aes_string(x = date.index, y = "value", group = "variable"), colour = "black") +
       ggplot2::ylab(dependent.var)
   }
-  
   print(lm.plot)
 }
