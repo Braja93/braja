53f26584bee4033d6c80bd17f33a5d90c5a25c99
nladwa <nitin.ladwa@gmail.com>
2017-01-06 16:41:25 +0000

    Changed gam.contribution.ggplot to take a GAM object - now lets you add intercept to the partial residual plots, y-axis to be interpreted as marginal contribution(?).

--- R/gam.contribution.ggplot.R
+++ R/gam.contribution.ggplot.R
@@ -1,30 +1,37 @@
 #' Plots the contribution of a GAM's smoothed independent variables
 #'
-#' Takes gam.contribution output and visualises it.
-#' @param gam.contibution.object The output of gam.contribution().
+#' Takes GAM and visualises it.
+#' @param gam.object GAM object. A GAM saved to a variable.
 #' @keywords gam contribution ggplot
 #' @import ggplot2
 #' @export
 #' @example inst/examples/gam.contribution.ggplot.R
 
-gam.contribution.ggplot <- function(gam.contribution.object, scales = "free_x") {
+gam.contribution.ggplot <- function (gam.object, incl.intercept = TRUE, scales = "free_x") {
   
-  dt <- copy(gam.contribution.object)
+  int <- coef(gam.object)["(Intercept)"]
   
-  lapply(dt, function(zzz) { zzz[, variable := names(zzz)[1] ] })
+  if(is.na(int)) { int <- 0; message("No intercept in model") }
   
+  dt <- gam.contribution(gam.object)
+  lapply(dt, function(zzz) {
+    zzz[, `:=`(variable, names(zzz)[1])]
+  })
   lapply(seq_along(dt), function(i) {
     old.names <- names(dt[[i]])
     new.names <- c("xvar", "contribution", "variable")
-    data.table::setnames(x = dt[[i]], old = old.names, new = new.names) })
+    data.table::setnames(x = dt[[i]], old = old.names, new = new.names)
+  })
   
-  out <- Reduce(function(...) merge(..., all = TRUE), dt) # merge all decomps
+  out <- Reduce(function(...) merge(..., all = TRUE), dt)
   
-  p1 <- ggplot2::ggplot(out, ggplot2::aes(x = xvar, y = contribution, colour = variable)) +
-    ggplot2::geom_hline(yintercept = 0, alpha = 0.3) +
-    ggplot2::geom_line() +
-    ggplot2::facet_wrap(~variable, ncol = 3, scales = scales) +
-    labs(x = "")
+  if(isTRUE(incl.intercept)) {out[, contribution := contribution + int]}
+  
+  p1 <- ggplot2::ggplot(out, ggplot2::aes(x = xvar, y = contribution, 
+                                          colour = variable)) + ggplot2::geom_hline(yintercept = 0, 
+                                                                                    alpha = 0.3) + ggplot2::geom_line() + ggplot2::facet_wrap(~variable, 
+                                                                                                                                              ncol = 3, scales = scales) + labs(x = "")
+  
   
   print(p1)
 }
