89884c2e1c41fa15568312dabfc701fb821c9377
nladwa <nitin.ladwa@gmail.com>
2017-03-10 17:17:50 +0000

    Updated corPlot.

--- R/corPlot.R
+++ R/corPlot.R
@@ -8,18 +8,25 @@
 #' @export
 #' @example inst/examples/corPlot.R
 
-corPlot <- function(x) {
+corPlot <- function(x, use = "complete.obs") {
   
-  out <- cor(Filter(is.numeric, x))
+  out <- Filter(is.numeric, x)
+  
+  if(use == "complete.obs") {
+    out <- out[complete.cases(out)]
+  } 
+  
+  obs <- paste0("n. obs = ", nrow(out))
+  
+  out <- cor(out, use = use)
   
   corplot <- data.table::melt(out)
   
-  p1 <- ggplot2::ggplot(corplot, ggplot2::aes(Var1, Var2, fill = value)) +
+  p1 <- ggplot2::ggplot(corplot, ggplot2::aes(Var1, Var2, fill = value)) + 
     ggplot2::geom_tile() +
     ggplot2::scale_fill_gradient2("Correlation", limits = c(-1, 1)) +
     ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1)) +
-    ggplot2::labs(list(x = "", y = ""))
+    ggplot2::labs(list(x = "", y = "", caption = obs))
   
   print(p1)
-  
 }
