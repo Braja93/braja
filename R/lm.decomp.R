e391062516ec295043015fb039dee568b5839ae8
nladwa <nitin.ladwa@gmail.com>
2017-03-22 13:37:33 +0000

    lm.decomp - updated to sort by Proportion, descending.

--- R/lm.decomp.R
+++ R/lm.decomp.R
@@ -11,15 +11,13 @@ lm.decomp <- function(lm.contribution.object) {
   out <- as.data.table(out, keep.rownames = TRUE)
   setnames(out, names(out), c("Variable", "Decomp"))
   dependent.var <- out[1, Variable]
-  out <- out[-1,]
+  out <- out[-1, ]
   
-  out$Proportion <- out$Decomp / sum(out$Decomp)
-  out$Proportion <- round(out$Proportion, digits = 4)
+  out[, Proportion := Decomp / sum(Decomp)]
+  out[, Proportion := round(Proportion, digits = 4)]
+  out <- out[order(-Proportion)]
   
-  out <- rbind(data.table(Variable = paste0("Dependent Variable = ", dependent.var),
-                   Decomp = NA,
-                   Proportion = NA),
-        out)
+  out <- rbindlist(list(data.table(Variable = paste0("Dependent Variable = ", dependent.var), Decomp = NA, Proportion = NA), out))
   
   return(out)
 }
